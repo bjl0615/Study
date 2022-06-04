66ba9321300a0b0fe06446f2366f0c23301b802e
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-06-04 22:29:08 +0900

    Update Station

---
+++ Bus/src/Bus/Station.java
@@ -0,0 +1,33 @@
+package Bus;
+
+public class Station /* extends Route */{
+	String name;
+	int id;
+	
+	public Station() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Station(String name , int id) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return "역 이름 : " + name + " 역 id : " + id;
+	}
+}
