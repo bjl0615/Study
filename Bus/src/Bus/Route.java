39d27008f36a4185b5b7c59708dc4d79ef9d5ffe
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-06-04 22:28:57 +0900

    Route

---
+++ Bus/src/Bus/Route.java
@@ -0,0 +1,38 @@
+package Bus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Route{
+	List<Station> route;
+	
+	public Route(Station st) {
+		route = new ArrayList<Station>();
+		route.add(st);
+	}
+	
+	public void addStation(Station st) {
+		route.add(st);
+	}
+	
+//	public void removeStaion(Station st) {
+//		route.remove(st);
+//	}
+//	
+//	public void updateStaion(String st1 , Station st2) {
+//			
+//	}
+	public List<Station> getRoute() {
+		return route;
+	}
+	public void setRoute(List<Station> route) {
+		this.route = route;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "역이 " + Integer.toString(route.size()) + "개 있습니다.";
+		
+	}
+}
